7c0fbc006aae184cbdd3793f67a92e852216c83a
Lydia Scarf <lydia@oldgrowth.city>
2026-04-23 13:39:48 +0200

    Add sorting to the city tracker page

--- scripts/src/components/city-tracker-overview.js
+++ scripts/src/components/city-tracker-overview.js
@@ -1,3 +1,4 @@
+import $ from 'jquery'
 import { defaults, filter, pick } from 'lodash'
 import TmplCityTrackerHeader from '../templates/city-tracker-header'
 import TmplCityTrackerTable from '../templates/city-tracker-table'
@@ -5,20 +6,43 @@ import {createDatasetFilters, queryByHook, setContent, slugify} from '../util'
 
 export default class {
   constructor (opts) {
-    const elements = {
+    this.elements = {
       cityTrackerHeader: queryByHook('city-tracker-header', opts.el),
       cityTrackerTable: queryByHook('city-tracker-table', opts.el),
     }
-    if (!opts.params.city) {
+    this.cities = opts.cities;
+    this.datasets = opts.datasets;
+    this.datatypes = opts.datatypes;
+    this.params = opts.params;
+    this.sortField = 'category';
+    this.sortDirection = 'asc';
+
+    if (!this.params.city) {
       const firstCity = opts.cities[0]
-      opts.params.city = slugify(firstCity.city_id)
+      this.params.city = slugify(firstCity.city_id)
     }
-    
-    const paramFilters = pick(opts.params, ['datatypeCategory', 'city'])
+
+    const paramFilters = pick(this.params, ['datatypeCategory', 'city'])
     const attributeFilters = pick(opts.el.data(), ['datatypeCategory', 'city'])
     const filters = createDatasetFilters(defaults(paramFilters, attributeFilters))
-    const filteredDatasets = filter(opts.datasets, filters)
+    const filteredDatasets = filter(this.datasets, filters)
     
+    this.cityStats = this._calculateCityStats(filteredDatasets);
+
+    const filteredDatatypes = defaults(paramFilters, attributeFilters).datatypeCategory
+      ? this.datatypes.filter(dt => slugify(dt.category) === defaults(paramFilters, attributeFilters).datatypeCategory)
+      : this.datatypes;
+    this.cityDatatypes = filteredDatatypes.map(datatype => {
+      const dataset = filteredDatasets.find(d => d.datatypes && d.datatypes.some(dt => dt.title === datatype.title))
+      return {
+        datatype: datatype,
+        dataset: dataset,
+      }
+    })
+    this._render()
+  }
+
+  _calculateCityStats(filteredDatasets) {
     const stats = filteredDatasets.reduce((acc, dataset) => {
       const datatypes = dataset.datatypes || [];
       if (dataset.is_partial || dataset.is_unavailable) {
@@ -31,46 +55,57 @@ export default class {
       countComplete: 0,
       countExcluded: 0,      
     });
-    const city = opts.cities.find(c => slugify(c.city_id) === opts.params.city);
-    const headerData = {
+    const city = this.cities.find(c => slugify(c.city_id) === slugify(this.params.city));
+    return {
       ...city,
       countComplete: stats.countComplete,
       countExcluded: stats.countExcluded,
-      coverage: (stats.countComplete / opts.datatypes.length * 100).toFixed(2)+"%"
+      coverage: (stats.countComplete / this.datatypes.length * 100).toFixed(2)+"%"
     }
-    const filteredDatatypes = defaults(paramFilters, attributeFilters).datatypeCategory
-      ? opts.datatypes.filter(dt => slugify(dt.category) === defaults(paramFilters, attributeFilters).datatypeCategory)
-      : opts.datatypes;
-    const cityDatatypes = filteredDatatypes.map(datatype => {
-      const dataset = filteredDatasets.find(d => d.datatypes && d.datatypes.some(dt => dt.title === datatype.title))
-      return {
-        datatype: datatype,
-        dataset: dataset,
+  }
+
+  _sortCityDatatypes(cityDatatypes) {
+    return cityDatatypes.sort((a, b) => {
+      let aVal;
+      let bVal;
+      
+      if (['category'].includes(this.sortField)) {
+        aVal = (a.datatype.category || '').toString().toLowerCase();
+        bVal = (b.datatype.category || '').toString().toLowerCase();
+      } else if (['datatype'].includes(this.sortField)) {
+        aVal = (a.datatype.title || '').toString().toLowerCase();
+        bVal = (b.datatype.title || '').toString().toLowerCase();
+      } else if (['dataset'].includes(this.sortField)) {
+        aVal = (a.dataset?.title || '').toString().toLowerCase();
+        bVal = (b.dataset?.title || '').toString().toLowerCase();
       }
-    }).sort((a, b) => {
-      if ( a.datatype.category < b.datatype.category ){
-        return -1;
-      }
-      if ( a.datatype.category > b.datatype.category ){
-        return 1;
-      }
-      if ( a.datatype.title < b.datatype.title ){
-        return -1;
-      }
-      if ( a.datatype.title > b.datatype.title ){
-        return 1;
-      }
-      if (a.dataset && b.dataset) {
-        if (a.dataset.title < b.dataset.title) {
-          return -1;
-        }
-        if (a.dataset.title > b.dataset.title) {
-          return 1;
-        }
-      }
+      
+      if (aVal < bVal) return this.sortDirection === 'asc' ? -1 : 1;
+      if (aVal > bVal) return this.sortDirection === 'asc' ? 1 : -1;
       return 0;
     })
-    setContent(elements.cityTrackerHeader, TmplCityTrackerHeader(headerData))
-    setContent(elements.cityTrackerTable, TmplCityTrackerTable(cityDatatypes))
+  }
+
+
+  _attachSortListeners() {
+    const headers = this.elements.cityTrackerTable.find('th[data-sort]');
+    headers.on('click', (e) => {
+      const field = $(e.currentTarget).data('sort');
+      
+      if (this.sortField === field) {
+        this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+      } else {
+        this.sortField = field;
+        this.sortDirection = 'asc';
+      }
+      
+      this._render();
+    });
+  }
+
+  _render() {
+    setContent(this.elements.cityTrackerHeader, TmplCityTrackerHeader(this.cityStats))
+    setContent(this.elements.cityTrackerTable, TmplCityTrackerTable({ cityDatatypes: this._sortCityDatatypes(this.cityDatatypes), sortField: this.sortField, sortDirection: this.sortDirection }))
+    this._attachSortListeners()
   }
 }
